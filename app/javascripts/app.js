68cf8b20d8a3d95c5e444f43b216bc2043fb6799
Fabibi <stepho.fabien@gmail.com>
2017-06-21 13:11:13 +0200

    No web3? You should consider trying MetaMask !

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -184,6 +184,8 @@ $( document ).ready(function() {
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
+	alert('No web3 ? You should consider trying MetaMask !')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
